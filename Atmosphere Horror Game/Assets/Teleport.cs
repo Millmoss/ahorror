64ece48fcb0e8633760e191ac3beba7b4adea3e0
Millmoss <omelliff@yahoo.com>
2019-03-17 21:16:33 -0400

    Prototyped non-euclidean space

---
+++ Atmosphere Horror Game/Assets/Teleport.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+	public GameObject port = null;
+
+	void OnTriggerEnter(Collider c)
+	{
+		if (c.tag == "Player")
+		{
+			Vector3 toPos = transform.position - c.transform.position;
+			c.transform.position = port.transform.position - toPos;
+		}
+	}
+}
